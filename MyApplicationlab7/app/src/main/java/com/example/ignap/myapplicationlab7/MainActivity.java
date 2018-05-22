1115a17c97c34d0b778ca7f5f00a989ccca6c797
ibparrague@miuandes.cl <ibparrague@miuandes.cl>
2018-05-22 12:33:44 -0400

    mini update

--- MyApplicationlab7/app/src/main/java/com/example/ignap/myapplicationlab7/MainActivity.java
+++ MyApplicationlab7/app/src/main/java/com/example/ignap/myapplicationlab7/MainActivity.java
@@ -1,16 +1,27 @@
 package com.example.ignap.myapplicationlab7;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private PendingIntent pendingIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getBaseContext(), "ASDASDASDASD", Toast.LENGTH_LONG).show();
 
+        Intent alarmIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, alarmIntent, 0);
+
+        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        int interval = 8000;
+
+        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, pendingIntent);
+        Toast.makeText(this, "Alarm Set", Toast.LENGTH_SHORT).show();
     }
 }
